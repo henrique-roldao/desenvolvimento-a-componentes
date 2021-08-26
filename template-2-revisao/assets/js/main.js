c0e9e9388cc1c848f3c7527307601ab534be49b7
henrique-roldao <henriqueroldao@rede.ulbra.br>
2021-08-26 20:32:36 -0300

    refactor(trocando os js para o painel administrador): migrando os JS para o template-2

---
+++ template-2-revisao/assets/js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+  home();
+
+  $("#home").click(function () {
+    home();
+  });
+
+  $("#features").click(function () {
+    features();
+  });
+
+  $("#contact").click(function () {
+    contact();
+  });
+});
+
+function home() {
+  $(".testeJs").html(" <div class='title'><h1>Bem vindo!</h1></div>");
+}
+
+function features() {
+  $(".testeJs").html("<div class='title'><h1>Features!</h1></div>");
+}
+
+function contact() {
+  $(".testeJs").load("views/contact.html");
+}
+
+function LoadPerson() {
+  var Person = {
+    name: document.querySelector("#name_form").value,
+    email: document.querySelector("#email_form").value,
+    phone: document.querySelector("#phone_form").value,
+    subject: document.querySelector("#subject_form").value,
+  };
+  $(".testeJs").load("views/person.html", function () {
+    var teste = document.querySelector("#namePerson");
+    teste.innerHTML = `${Person.name}`;
+
+    var teste = document.querySelector("#emailPerson");
+    teste.innerHTML = `${Person.email}`;
+
+    var teste = document.querySelector("#phonePerson");
+    teste.innerHTML = `${Person.phone}`;
+
+    var teste = document.querySelector("#subjectPerson");
+    teste.innerHTML = `${Person.subject}`;
+  });
+}
